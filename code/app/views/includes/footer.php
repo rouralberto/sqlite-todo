ebcc31de5c9f511891b337bab099b2fcdffb39c5
Alberto Roura <albertoroura@gmail.com>
2019-02-05 20:21:45 +1000

    offloaded bootstrap and jQuery. removed stuff

--- code/app/views/includes/footer.php
+++ code/app/views/includes/footer.php
@@ -5,8 +5,8 @@
 <?php require_once 'modals.php'; ?>
 
 <!-- JavaScript -->
-<script src="/assets/js/jquery-1.10.2.js"></script>
-<script src="/assets/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 <script src="/assets/plugins/select2/select2.min.js"></script>
 <script src="/assets/plugins/dataTables/jquery.dataTables.js"></script>
 <script src="/assets/plugins/dataTables/dataTables.bootstrap.js"></script>
--- code/app/views/includes/header.php
+++ code/app/views/includes/header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $title; ?></title>
-	<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/assets/css/font-awesome.min.css" rel="stylesheet">
 	<link href="/assets/plugins/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet">
 	<link href="/assets/plugins/jGrowl/jquery.jgrowl.css" rel="stylesheet">
@@ -13,6 +13,7 @@
 	<link href="/assets/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
 	<link href="/assets/plugins/summernote/summernote.css" rel="stylesheet">
 	<link href="/assets/css/custom.css" rel="stylesheet">
+	<link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAc/8AAAAAAABaxwAAX9QA////AABl4AAAbPAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEQAAAAAAAAEEUAAAAACIkERQiIgACIkEREUIiIDM0FEQRQzMzMzNDM0EUMzNVVVVVVBRVVVVVVVVVQUVVZmZmZmZkFGZmZmZmZmZGZgAAAAAAAAAAEAAAAAAAAAGAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAQAA" rel="icon" type="image/x-icon"/>
 </head>
 
 <body>
